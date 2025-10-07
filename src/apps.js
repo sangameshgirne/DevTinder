dfb9a466a6600b8ded2895b9e0633c61c052c065
Sangamesh Girne <sangameshgirne143@gmail.com>
2025-10-07 19:24:52 +0530

    route handler

--- src/apps.js
+++ src/apps.js
@@ -1,9 +1,21 @@
 const express=require("express");
 const app=express();
-app.get("/.*fly",(req,res)=>{
-res.send("HI");
 
-})
+app.use("/user",
+[(req,res,next)=>{
+console.log("handle the route user1");
+res.send("Response1");
+next();
+},
+
+(req,res,next)=>{
+console.log("handle the route user2");
+res.send("Response2");
+next();
+}],
+
+)
+
 
 
 app.listen(7777,()=>{
